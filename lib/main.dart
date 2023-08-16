412ba0571f326f0ce38c0536125c5237041c258d
antonymwakiacha <tonywugoma@gmail.com>
2023-08-16 22:07:24 +0300

    step 3

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
+import 'dart:developer' as devtools;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,7 +43,7 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print("Email is verified.");
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
@@ -50,21 +51,93 @@ class HomePage extends StatelessWidget {
               return const LoginView();
             }
 
-            return const Text("Done");
+          // return const Text("Done");
 
-            //   // print(user);
-            //   // //final emailVerified = user?.emailVerified ?? false;
-            //   // if (user?.emailVerified ?? false) {
-            //   //   return const Text('Done');
-            //   //   // print("You are a verified user.");
-            //   // } else {
-            //   //   return const VerifyEmailView();
-            //   // }
-            // return const LoginView();
+          //   // print(user);
+          //   // //final emailVerified = user?.emailVerified ?? false;
+          //   // if (user?.emailVerified ?? false) {
+          //   //   return const Text('Done');
+          //   //   // print("You are a verified user.");
+          //   // } else {
+          //   //   return const VerifyEmailView();
+          //   // }
+          // return const LoginView();
           default:
             return const CircularProgressIndicator();
         }
       },
     );
   }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main UI"),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/',
+                      (_) => false,
+                    );
+                  }
+                  devtools.log(shouldLogout.toString());
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Log out'),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text("Hello World"),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Sign out"),
+        content: const Text("Are you sure you want to sign out?"),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log Out')),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
 }
